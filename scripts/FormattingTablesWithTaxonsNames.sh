5999a9dbc64b021e21f833fc8747491ad8f86ac6
Joao Pedro <jpcastrodarocha@gmail.com>
2026-03-17 14:58:55 -0300

    Restructuring Project Directory Organization. Commiting non-usual directorys, like OldFiles to save them inside a commit (and then remove this files)

--- scripts/FormattingTablesWithTaxonsNames.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# One-liner to remove the duplicates rows that have the first field being different from the forth field (if the field is 
-# consider lowecase)
-
-tail -n +2 TaxonsFromLiterature_Bac.txt | taxonkit name2taxid -i 2 -r > temp.tsv && awk -F '\t' 'FNR==NR {count[$2]++; next} 
-!(count[$2] > 1 && tolower($1) !~ tolower($4))' temp.tsv temp.tsv | column -t -s $'\t' | less
